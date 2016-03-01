eb698cd01c77a4875d3b59a6bb750470aa0c955b
serhiiwinl <sergiiwinl@gmail.com>
2016-03-02 01:00:27 +0200

    added correct  jetty-server dependencies

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,11 @@
+import org.eclipse.jetty.server.Server;
+
 /**
  * Created by Sergii on 02.03.2016.
  */
 public class Main {
     public static void main(String[] args) {
+        Server server = new Server(8080);
         System.out.println("start");
     }
 }
